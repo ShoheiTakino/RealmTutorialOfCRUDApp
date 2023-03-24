597ef9a5a3a4b8b5a2b92212a68a022de546ab31
ShoheiTakino <shohei0522penguin@gmail.com>
2023-03-24 19:30:11 +0900

    Initial Commit

---
+++ RealmTutorialOfCRUDApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTutorialOfCRUDApp
+//
+//  Created by 滝野翔平 on 2023/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
