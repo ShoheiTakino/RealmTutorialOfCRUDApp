bee315277bf47e12b49479c8618071664812a1a7
ShoheiTakino <shohei0522penguin@gmail.com>
2023-05-05 21:09:24 +0900

    komennto

--- RealmTutorialOfCRUDApp/RealmDBConverter.swift
+++ RealmTutorialOfCRUDApp/RealmDBConverter.swift
@@ -10,6 +10,10 @@ import RealmSwift
 
 struct RealmDBConverter {
     
+    
+    /// Results型で取得することになっているデータを任意の型に形成する処理
+    /// - Parameter object: RealmのResults型で返却される値
+    /// - Returns: RealmのResults型から形成されたされた任意の型の配列
     static func convertToModelFrom<T>(object: Results<T>) -> [T] {
         var list: [T] = []
         for i in 0..<object.count {
